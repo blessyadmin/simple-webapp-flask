c7ac99e6e4f8b35ec48f0f398358cb4fe40915ee
blessyadmin <blessyaru07@gmail.com>
2023-10-12 20:34:45 +0530

    changes done

--- app.py
+++ app.py
@@ -10,6 +10,14 @@ def main():
 def hello():
     return 'I am good, how about you?'
 
+    @app.route("/who are you")
+def main():
+    return "iam ARUNA!"
+
+    @app.route('/where r u from')
+def hello():
+    return 'IAM FROM VIZAG'
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
